0e8a49d21ad83bb3e6f7a462d63a1b9dc97beeb2
Hoon9901 <shonn.dev@gmail.com>
2022-04-26 22:10:15 +0900

    delete testresult domain

--- src/main/java/com/example/codebase/domain/testresult/repository/ResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.codebase.domain.testresult.repository;
-
-import com.example.codebase.domain.testresult.entity.TestResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultRepository extends JpaRepository<TestResult, Long> {
-}
