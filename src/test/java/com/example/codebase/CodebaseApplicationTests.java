a856d3366af6c0fd9f625543c14ca527290656b7
Hoon9901 <shonn.dev@gmail.com>
2022-04-26 22:07:20 +0900

    modify dockerfile

--- src/test/java/com/example/codebase/CodebaseApplicationTests.java
+++ src/test/java/com/example/codebase/CodebaseApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BraincommunicationBackendApplicationTests {
+class CodebaseApplicationTests {
 
     @Test
     void contextLoads() {
