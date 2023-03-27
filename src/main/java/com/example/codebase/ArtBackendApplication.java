3fa9d7903a86a8737e56b60b70f0fd09e8dfcff3
Hoon9901 <shonn.dev@gmail.com>
2023-03-27 21:31:10 +0900

    chore: Project 명 변경

--- src/main/java/com/example/codebase/ArtBackendApplication.java
+++ src/main/java/com/example/codebase/ArtBackendApplication.java
@@ -8,11 +8,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class CodebaseApplication {
+public class ArtBackendApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(CodebaseApplication.class, args);
+        SpringApplication.run(ArtBackendApplication.class, args);
     }
 
     @Bean
