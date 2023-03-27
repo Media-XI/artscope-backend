aa008277c4168b3faace3535b1c58622f7ba72de
Hoon9901 <shonn.dev@gmail.com>
2023-03-27 20:20:04 +0900

    chore: MySQL Database명 재지정 (default -> art)

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO authority (authority_name) values ('ROLE_USER');
+INSERT INTO authority (authority_name) values ('ROLE_ARTIST');
 INSERT INTO authority (authority_name) values ('ROLE_ADMIN');
