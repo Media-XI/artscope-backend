ff737ff73fc6256e5afdd33fd391137d3974b519
Hoon9901 <shonn.dev@gmail.com>
2023-03-27 21:18:18 +0900

    feat: Add RedisUtil

---
+++ src/main/java/com/example/codebase/util/RedisUtil.java
@@ -0,0 +1,37 @@
+package com.example.codebase.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@Slf4j
+public class RedisUtil {
+
+    private final StringRedisTemplate redisTemplate;
+
+    @Autowired
+    public RedisUtil(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    // 유효시간 동안 Data 저장
+    public void setDataAndExpire(String key, String value, long duration) { // 유효시간은 밀리세컨드 단위
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofMillis(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+}
