2bf1204c57cf85d22662acf2add24e8383f2fe18
Hoon9901 <shonn.dev@gmail.com>
2023-04-04 14:38:19 +0900

    chore: swagger multipart 적용

--- src/main/java/com/example/codebase/controller/UploadController.java
+++ src/main/java/com/example/codebase/controller/UploadController.java
@@ -3,11 +3,9 @@ package com.example.codebase.controller;
 import com.example.codebase.s3.S3Service;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -22,9 +20,9 @@ public class UploadController {
         this.s3Service = s3Service;
     }
 
-    @ApiOperation(value = "파일 업로드", notes = "[USER] 파일 업로드")
-    @PostMapping
-    public ResponseEntity upload(@RequestParam MultipartFile multipartFile) throws IOException {
+    @ApiOperation(value = "파일 하나 업로드", notes = "[USER] 파일 업로드")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity upload(@RequestPart MultipartFile multipartFile) throws IOException {
         return new ResponseEntity(s3Service.saveUploadFile(multipartFile), HttpStatus.CREATED);
     }
 }
