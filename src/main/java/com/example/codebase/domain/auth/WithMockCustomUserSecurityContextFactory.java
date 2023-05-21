3fb55e4f017465dfe1286c11f0ad74716a0a55c8
Hoon9901 <shonn.dev@gmail.com>
2023-05-21 16:37:39 +0900

    refactor: @WithMockCustomUser 어노테이션의 여러개 Role을 지정할 수 있도록 개선

--- src/main/java/com/example/codebase/domain/auth/WithMockCustomUserSecurityContextFactory.java
+++ src/main/java/com/example/codebase/domain/auth/WithMockCustomUserSecurityContextFactory.java
@@ -7,14 +7,21 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
     @Override
     public SecurityContext createSecurityContext(WithMockCustomUser annotation) {
         final SecurityContext context = SecurityContextHolder.createEmptyContext();
 
+        String[] roles = annotation.role().split(",");
+        final List<SimpleGrantedAuthority> authorities = Arrays.stream(roles)
+                .map((role) -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+
         final UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                annotation.username(), "password", Arrays.asList(new SimpleGrantedAuthority("ROLE_" + annotation.role())));
+                annotation.username(), "password", authorities);
         context.setAuthentication(authenticationToken);
         return context;
     }
