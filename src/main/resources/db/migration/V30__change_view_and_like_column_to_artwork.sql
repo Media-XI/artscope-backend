fe5208c2f34925228d39a1dc29c3531fa925e279
Hoon9901 <shonn.dev@gmail.com>
2023-05-21 17:21:04 +0900

    fix: sql artwowk view, like sql 적용안되는 현상 수정

--- src/main/resources/db/migration/V30__change_view_and_like_column_to_artwork.sql
+++ src/main/resources/db/migration/V30__change_view_and_like_column_to_artwork.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `artwork`
-    CHANGE COLUMN `view` `views` INTEGER NOT NULL DEFAULT 0 AFTER `description`;
-    CHANGE COLUMN `like` `likes` INTEGER NOT NULL DEFAULT 0 AFTER `view`;
+    CHANGE COLUMN `view` `views` INTEGER NOT NULL DEFAULT 0;
+
+ALTER TABLE `artwork`
+    CHANGE COLUMN `like` `likes` INTEGER NOT NULL DEFAULT 0;
