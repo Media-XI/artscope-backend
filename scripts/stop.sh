c53bfedb6373e7d8219daa14dc77c3d43ba7b23f
Hoon9901 <shonn.dev@gmail.com>
2023-07-26 18:06:47 +0900

    chore: CodeDeploy stop.sh ID 기반에서 Name으로 변경
    - 컨테이너 ID를 통해 중지, 삭제 하던것을 컨테이너 이름으로 변경함

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,5 +8,5 @@ echo "실행중인 컨테이너 ID: $RUNNING_CONTAINER_ID"
 # 이전 도커 컨테이너 종료
 if [ -n "$RUNNING_CONTAINER_ID" ]; then
   echo "이전 도커 컨테이너 종료 및 삭제합니다"
-  docker stop $RUNNING_CONTAINER_ID && docker rm $RUNNING_CONTAINER_ID
+  docker stop CONTAINER_NAME && docker rm CONTAINER_NAME
 fi
