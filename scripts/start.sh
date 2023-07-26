6c6d99de5d0a4e60aad75820aa61a1da51973e1d
Hoon9901 <shonn.dev@gmail.com>
2023-07-26 17:35:33 +0900

    feat: AWS CodeDeploy Start.sh env file load 옵션 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,5 @@ CONTAINER_NAME=art-backend
 
 # 새로운 도커 컨테이너 실행
 echo "IMAGE_NAME: $IMAGE_NAME 도커 실행"
-docker run -d -p 18080:8080 --name $CONTAINER_NAME $IMAGE_NAME
+docker run -d -p 18080:8080 --name $CONTAINER_NAME $IMAGE_NAME \
+  --env-file /home/ubuntu/env/backend.env
