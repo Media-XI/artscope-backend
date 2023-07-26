1e2a10949075e80f19ed01697be4f2159beccc1c
Hoon9901 <shonn.dev@gmail.com>
2023-07-26 17:47:41 +0900

    feat: CodeDeploy start.sh env 파일명 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ CONTAINER_NAME=art-backend
 # 새로운 도커 컨테이너 실행
 echo "IMAGE_NAME: $IMAGE_NAME 도커 실행"
 docker run -d -p 18080:8080 --name $CONTAINER_NAME $IMAGE_NAME \
-  --env-file /home/ubuntu/env/backend.env
+  --env-file /home/ubuntu/env/.env
