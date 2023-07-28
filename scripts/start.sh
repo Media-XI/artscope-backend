bfcd0de29316010142e736a1cfcf6c8195d8bff8
Hoon9901 <shonn.dev@gmail.com>
2023-07-28 15:41:20 +0900

    feat: CodeDeploy start.sh 컨테이너 생성 방식 변경
    - docker run으로 하니 환경변수를 재사용하는 부분이 적용이 안됨
    - docker-compose를 사용함으로써 해결

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,60 @@
 IMAGE_FILE_PATH="/home/ubuntu/deploy/image.txt"
 IMAGE_NAME=$(cat "$IMAGE_FILE_PATH")
-CONTAINER_NAME=art-backend
 CONTAINER_ENV_PATH="/home/ubuntu/env/.env"
+SERVICE_NAME=art-be
 
 # 새로운 도커 컨테이너 실행
 echo "IMAGE_NAME: $IMAGE_NAME 도커 실행"
-docker run -d -p 18080:8080 --name $CONTAINER_NAME \
-  --env-file $CONTAINER_ENV_PATH \
- $IMAGE_NAME
+echo "version: '3.8'
+
+services:
+  art-be:
+    container_name: art-backend
+    image: ${IMAGE_NAME}
+    ports:
+      - 18080:8080
+    env-file:
+      - ${CONTAINER_ENV_PATH}
+    volumes:
+      - logs_data:/logs
+      - ./pinpoint/profiles:/pinpoint-agent/profiles:rw
+      - pinpoint-volumes:/pinpoint-agent
+    depends_on:
+      - redis
+      - pinpoint-agent
+
+  pinpoint-agent:
+    image: pinpointdocker/pinpoint-agent:2.5.2
+    volumes:
+      - pinpoint-volumes:/pinpoint-agent
+
+  redis:
+    image: redis:7.0-alpine
+    command: redis-server --port 6379
+    container_name: art-redis
+    labels:
+      - "name=redis"
+      - "mode=standalone"
+    ports:
+      - 16379:6379
+
+  filebeat:
+    image: filebeat-custom:0.18
+    container_name: art-filebeat
+    volumes:
+      - logs_data:/logs
+    ports:
+      - 5044:5044
+    depends_on:
+      - art-be
+
+volumes:
+  logs_data:
+  pinpoint-volumes:
+
+networks:
+  default:
+    name: art
+    external: true" > docker-compose.yaml
+
+docker-compose up -d $SERVICE_NAME
