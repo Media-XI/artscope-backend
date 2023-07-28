6801bbef60474702fcb6f6d26037833bcc2473a5
Hoon9901 <shonn.dev@gmail.com>
2023-07-28 14:54:55 +0900

    fix: CodeDeploy scripts/start.sh 환경변수를 불러오지 못한 현상 해결
    - docker run의 명령어 배치 순서를 변경하여 --env-file로 컨테이너 환경변수를 불러올 수 있도록 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,10 @@
 IMAGE_FILE_PATH="/home/ubuntu/deploy/image.txt"
 IMAGE_NAME=$(cat "$IMAGE_FILE_PATH")
 CONTAINER_NAME=art-backend
+CONTAINER_ENV_PATH="/home/ubuntu/env/.env"
 
 # 새로운 도커 컨테이너 실행
 echo "IMAGE_NAME: $IMAGE_NAME 도커 실행"
-docker run -d -p 18080:8080 --name $CONTAINER_NAME $IMAGE_NAME \
-  --env-file /home/ubuntu/env/.env
+docker run -d -p 18080:8080 --name $CONTAINER_NAME \
+  --env-file $CONTAINER_ENV_PATH \
+ $IMAGE_NAME
