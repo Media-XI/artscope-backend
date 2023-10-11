60dc9eb9ecab176b0115c4b4cf8691966ce1f2ac
Hoon9901 <shonn.dev@gmail.com>
2023-10-11 15:46:44 +0900

    fix: PostWithLikesResponseDTO Builder 삭제
    - 상속으로 인한 Builder 중복

--- src/main/java/com/example/codebase/domain/post/dto/PostWithLikesResponseDTO.java
+++ src/main/java/com/example/codebase/domain/post/dto/PostWithLikesResponseDTO.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class PostWithLikesResponseDTO extends PostResponseDTO {
 
     List<PostLikeMemberDTO> likeMembers;
