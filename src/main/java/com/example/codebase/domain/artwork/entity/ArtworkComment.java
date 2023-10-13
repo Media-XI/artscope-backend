41ee30145c269d5c7d6d58c3cc1a84c7668b720f
Hoon9901 <shonn.dev@gmail.com>
2023-10-13 14:35:08 +0900

    feat: Artwork Comment Entity 구현 및 SQL 작성

---
+++ src/main/java/com/example/codebase/domain/artwork/entity/ArtworkComment.java
@@ -0,0 +1,56 @@
+package com.example.codebase.domain.artwork.entity;
+
+import com.example.codebase.domain.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "artwork_comment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class ArtworkComment {
+
+    @Id
+    @Column(name = "artwork_comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artwork_id")
+    private Artwork artwork;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private ArtworkComment parentComment;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL)
+    private List<ArtworkComment> childComments = new ArrayList<>();
+
+    @Column(name = "created_time")
+    private LocalDateTime createdTime;
+
+    @Column(name = "updated_time")
+    private LocalDateTime updatedTime;
+
+    public void addChildComment(ArtworkComment childComment) {
+        this.childComments.add(childComment);
+        childComment.parentComment = this;
+    }
+}
