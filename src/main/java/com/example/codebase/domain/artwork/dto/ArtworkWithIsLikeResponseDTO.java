45aeb463a15d9eb2c7f1305a4ceb8958f94a9878
Hoon9901 <shonn.dev@gmail.com>
2023-10-20 17:21:16 +0900

    refactor: ArtworkWithIsLikeResponseDTO isLike 변수명 변경
    - 좋아요 여부 변수를 isLiked로 변경

--- src/main/java/com/example/codebase/domain/artwork/dto/ArtworkWithIsLikeResponseDTO.java
+++ src/main/java/com/example/codebase/domain/artwork/dto/ArtworkWithIsLikeResponseDTO.java
@@ -14,33 +14,33 @@ public class ArtworkWithIsLikeResponseDTO {
 
     ArtworkResponseDTO artwork;
 
-    Boolean isLike;
+    Boolean isLiked;
 
     public static ArtworkWithIsLikeResponseDTO from(ArtworkWithIsLike artworkWithIsLike) {
         ArtworkWithIsLikeResponseDTO artworkWithIsLikeResponseDTO = new ArtworkWithIsLikeResponseDTO();
         artworkWithIsLikeResponseDTO.setArtwork(ArtworkResponseDTO.from(artworkWithIsLike.getArtwork()));
-        artworkWithIsLikeResponseDTO.setIsLike(artworkWithIsLike.getIsLike());
+        artworkWithIsLikeResponseDTO.setIsLiked(artworkWithIsLike.getIsLike());
         return artworkWithIsLikeResponseDTO;
     }
 
     public static ArtworkWithIsLikeResponseDTO from(Artwork artwork) {
         ArtworkWithIsLikeResponseDTO artworkWithIsLikeResponseDTO = new ArtworkWithIsLikeResponseDTO();
         artworkWithIsLikeResponseDTO.setArtwork(ArtworkResponseDTO.from(artwork));
-        artworkWithIsLikeResponseDTO.setIsLike(false);
+        artworkWithIsLikeResponseDTO.setIsLiked(false);
         return artworkWithIsLikeResponseDTO;
     }
 
     public static ArtworkWithIsLikeResponseDTO from(Artwork artwork, boolean existLike) {
         ArtworkWithIsLikeResponseDTO artworkWithIsLikeResponseDTO = new ArtworkWithIsLikeResponseDTO();
         artworkWithIsLikeResponseDTO.setArtwork(ArtworkResponseDTO.from(artwork));
-        artworkWithIsLikeResponseDTO.setIsLike(existLike);
+        artworkWithIsLikeResponseDTO.setIsLiked(existLike);
         return artworkWithIsLikeResponseDTO;
     }
 
     public static ArtworkWithIsLikeResponseDTO from(Artwork artwork, List<ArtworkCommentResponseDTO> comments, boolean existLike) {
         ArtworkWithIsLikeResponseDTO artworkWithIsLikeResponseDTO = new ArtworkWithIsLikeResponseDTO();
         artworkWithIsLikeResponseDTO.setArtwork(ArtworkResponseDTO.of(artwork, comments));
-        artworkWithIsLikeResponseDTO.setIsLike(existLike);
+        artworkWithIsLikeResponseDTO.setIsLiked(existLike);
         return artworkWithIsLikeResponseDTO;
     }
 }
