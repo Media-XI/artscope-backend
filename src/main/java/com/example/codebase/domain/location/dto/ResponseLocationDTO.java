1408c5839f4d64308de212072acc51e94396824e
haroya01 <haroya01@naver.com>
2023-11-03 12:36:57 +0900

    refactor: ResponseLocationDTO에 builder패턴 추가

--- src/main/java/com/example/codebase/domain/location/dto/ResponseLocationDTO.java
+++ src/main/java/com/example/codebase/domain/location/dto/ResponseLocationDTO.java
@@ -2,11 +2,17 @@ package com.example.codebase.domain.location.dto;
 
 import com.example.codebase.domain.exhibition.entity.EventSchedule;
 import com.example.codebase.domain.location.entity.Location;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseLocationDTO {
 
   private Long id;
@@ -30,32 +36,32 @@ public class ResponseLocationDTO {
   private String snsUrl;
 
   public static ResponseLocationDTO from(EventSchedule eventSchedule) {
-    ResponseLocationDTO dto = new ResponseLocationDTO();
-    dto.setId(eventSchedule.getLocation().getId());
-    dto.setLatitude(eventSchedule.getLocation().getLatitude());
-    dto.setLongitude(eventSchedule.getLocation().getLongitude());
-    dto.setAddress(eventSchedule.getLocation().getAddress());
-    dto.setName(eventSchedule.getLocation().getName());
-    dto.setEnglishName(eventSchedule.getLocation().getEnglishName());
-    dto.setLink(eventSchedule.getLocation().getLink());
-    dto.setPhoneNumber(eventSchedule.getLocation().getPhoneNumber());
-    dto.setWebSiteUrl(eventSchedule.getLocation().getWebSiteUrl());
-    dto.setSnsUrl(eventSchedule.getLocation().getSnsUrl());
-    return dto;
+    return ResponseLocationDTO.builder()
+        .id(eventSchedule.getLocation().getId())
+        .latitude(eventSchedule.getLocation().getLatitude())
+        .longitude(eventSchedule.getLocation().getLongitude())
+        .address(eventSchedule.getLocation().getAddress())
+        .name(eventSchedule.getLocation().getName())
+        .englishName(eventSchedule.getLocation().getEnglishName())
+        .link(eventSchedule.getLocation().getLink())
+        .phoneNumber(eventSchedule.getLocation().getPhoneNumber())
+        .webSiteUrl(eventSchedule.getLocation().getWebSiteUrl())
+        .snsUrl(eventSchedule.getLocation().getSnsUrl())
+        .build();
   }
 
   public static ResponseLocationDTO of(Location location) {
-    ResponseLocationDTO dto = new ResponseLocationDTO();
-    dto.setId(location.getId());
-    dto.setLatitude(location.getLatitude());
-    dto.setLongitude(location.getLongitude());
-    dto.setAddress(location.getAddress());
-    dto.setName(location.getName());
-    dto.setEnglishName(location.getEnglishName());
-    dto.setLink(location.getLink());
-    dto.setPhoneNumber(location.getPhoneNumber());
-    dto.setWebSiteUrl(location.getWebSiteUrl());
-    dto.setSnsUrl(location.getSnsUrl());
-    return dto;
+    return ResponseLocationDTO.builder()
+        .id(location.getId())
+        .latitude(location.getLatitude())
+        .longitude(location.getLongitude())
+        .address(location.getAddress())
+        .name(location.getName())
+        .englishName(location.getEnglishName())
+        .link(location.getLink())
+        .phoneNumber(location.getPhoneNumber())
+        .webSiteUrl(location.getWebSiteUrl())
+        .snsUrl(location.getSnsUrl())
+        .build();
   }
 }
