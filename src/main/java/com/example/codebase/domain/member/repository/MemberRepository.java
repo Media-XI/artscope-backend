526627fdc2e24cbcf220dc21df37e734bad3ee1c
Hoon9901 <shonn.dev@gmail.com>
2023-11-08 15:32:31 +0900

    style: code 포맷팅

--- src/main/java/com/example/codebase/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/codebase/domain/member/repository/MemberRepository.java
@@ -2,20 +2,22 @@ package com.example.codebase.domain.member.repository;
 
 
 import com.example.codebase.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
 
-    /*
+    /**
      * Deprecated!
      * Authorities를 Fetch.Lazy 에서 Eager로 변경하여 지연 로딩 없이 가져오므로
      * 해당 메소드는 사용하지 않아도 됨.
      */
+    @Deprecated
     Optional<Member> findOneWithAuthoritiesByUsername(String username);
 
     Optional<Member> findByUsername(String username);
