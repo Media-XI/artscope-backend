1d83660bf53c9fe7052514b34cdc05c0dfb2c85f
haroya01 <haroya01@naver.com>
2023-11-10 16:25:04 +0900

    fix: event_schedule 조회 문제 해결
    - exhibition을 조회할때 eventSchedule 여러개가 조회되던 문제해결

--- src/main/java/com/example/codebase/domain/exhibition/repository/ExhibitionRepository.java
+++ src/main/java/com/example/codebase/domain/exhibition/repository/ExhibitionRepository.java
@@ -1,7 +1,33 @@
 package com.example.codebase.domain.exhibition.repository;
 
+import com.example.codebase.domain.exhibition.entity.EventType;
 import com.example.codebase.domain.exhibition.entity.Exhibition;
+import java.time.LocalDateTime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
+
+  @Query(
+      "SELECT e "
+          + "FROM Exhibition e INNER JOIN EventSchedule es ON es.exhibition = e "
+          + "WHERE es.startTime >= :startLocalDateTime AND es.endTime <= :endLocalDateTime "
+          + "AND e.enabled = true "
+          + "ORDER BY es.startTime")
+  Page<Exhibition> findExhibitionsWithEventSchedules(
+      LocalDateTime startLocalDateTime, LocalDateTime endLocalDateTime, Pageable pageable);
+
+  @Query(
+      "SELECT e "
+          + "FROM Exhibition e INNER JOIN EventSchedule es ON es.exhibition = e "
+          + "WHERE es.startTime >= :startLocalDateTime AND es.endTime <= :endLocalDateTime "
+          + "AND e.enabled = true AND e.type = :eventType  "
+          + "ORDER BY es.startTime")
+  Page<Exhibition> findExhibitionsWithEventSchedules(
+      LocalDateTime startLocalDateTime,
+      LocalDateTime endLocalDateTime,
+      EventType eventType,
+      Pageable pageable);
 }
