f8f1c1bf6522ec7c3a373c3dc51c3df72360bc40
Hoon9901 <shonn.dev@gmail.com>
2023-11-16 21:27:52 +0900

    feat: Referer 조건 추가

--- src/main/java/com/example/codebase/filter/security/RefererCheckIntercepter.java
+++ src/main/java/com/example/codebase/filter/security/RefererCheckIntercepter.java
@@ -28,7 +28,7 @@ public class RefererCheckIntercepter implements HandlerInterceptor {
         }
 
         // referer가 없거나, host와 다르면 차단
-        if (referer == null || !referer.contains(host)) {
+        if (referer == null || !referer.contains(host) || !referer.contains("artscope.kr")) {
             log.info("유효하지 않은 Referer 요청 referer={}, host={}", referer, host);
             return false;
         }
