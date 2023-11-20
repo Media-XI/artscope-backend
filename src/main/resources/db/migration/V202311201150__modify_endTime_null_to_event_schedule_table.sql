b6f84b27ce4648d6ea0a4c4c6f76cf7e4f0fece7
gimdonghyeon <haroya01@naver.com>
2023-11-20 21:01:07 +0900

    fix: Drop Event Date Column 작업을 위한 SQL 변경 및 추가
    - 1. time -> dateTime 변경 작업 추가
    - 2. 변경된 dateTime에 date 추가하는 작업
    - 3. event_date 사용하지 않으므로 drop을 위한 과정 작업

--- src/main/resources/db/migration/V202311201150__modify_endTime_null_to_event_schedule_table.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE event_schedule MODIFY COLUMN end_time DATETIME NULL;
--- src/main/resources/db/migration/V202311201746__modify_start_date_end_date_to_event_schedule_table.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE event_schedule CHANGE COLUMN end_time end_date_time DATETIME NULL;
-ALTER TABLE event_schedule CHANGE COLUMN start_time start_date_time DATETIME NOT NULL;
