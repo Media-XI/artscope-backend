6c4cf72ea28e1fd2d02675866a30eff9216ef834
Hoon9901 <shonn.dev@gmail.com>
2023-11-22 14:52:45 +0900

    refactor: MemberRepository 사용하지 않는 쿼리 메소드 제거

--- src/main/java/com/example/codebase/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/codebase/domain/member/repository/MemberRepository.java
@@ -16,12 +16,6 @@ import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
 
-    /*
-     * Deprecated!
-     * Authorities를 Fetch.Lazy 에서 Eager로 변경하여 지연 로딩 없이 가져오므로
-     * 해당 메소드는 사용하지 않아도 됨.
-     */
-    Optional<Member> findOneWithAuthoritiesByUsername(String username);
 
     Optional<Member> findByUsername(String username);
 
