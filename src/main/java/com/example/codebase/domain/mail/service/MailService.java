d8728e2aae2ffc292f8cbdb7cf4d9dcf2274e39f
Hoon9901 <shonn.dev@gmail.com>
2023-12-05 18:40:24 +0900

    feat: 비밀번호 재설정 링크 메일 API 작성 및 아이디 찾기 메일 API 이동
    - 아이디 찾기 API를 MailController로 이동했습니다.

--- src/main/java/com/example/codebase/domain/mail/service/MailService.java
+++ src/main/java/com/example/codebase/domain/mail/service/MailService.java
@@ -1,5 +1,7 @@
 package com.example.codebase.domain.mail.service;
 
+import com.example.codebase.domain.member.entity.Member;
+import com.example.codebase.domain.member.exception.NotFoundMemberException;
 import com.example.codebase.domain.member.repository.MemberRepository;
 import com.example.codebase.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +19,14 @@ public class MailService {
 
     private final JavaMailSender javaMailSender;
     private final RedisUtil redisUtil;
-
     private final MemberRepository memberRepository;
 
     @Value("${mail.callback.url}")
     private String MAIL_CALLBACK;
+
+    @Value("${mail.password.callback.url}")
+    private String PASSWORD_RESET_CALLBACK;
+
 
     @Autowired
     public MailService(JavaMailSender javaMailSender, RedisUtil redisUtil, MemberRepository memberRepository) {
@@ -81,6 +86,48 @@ public class MailService {
         }
     }
 
+    public void sendPasswordResetMail(String email) {
+        memberRepository.findByEmail(email)
+                .orElseThrow(NotFoundMemberException::new);
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(email);
+            mimeMessageHelper.setSubject("[ArtScope] 비밀번호 재설정");
+
+            String code = UUID.randomUUID().toString().replace("-", "");
+            redisUtil.setDataAndExpire(code, email, 60 * 1000 * 5);
+
+            String sb = "<img src=\"" +
+                    randomUrl() +
+                    "\"/>" +
+                    "<h1>비밀번호 재설정</h1>" +
+                    "<h3>아래의 링크를 접속해주세요. </3>" +
+                    "<h3>재설정 링크: " +
+                    "<a href=\"" +
+                    PASSWORD_RESET_CALLBACK + "?code=" + code +
+                    "\">" +
+                    PASSWORD_RESET_CALLBACK + "?code=" + code +
+                    "</a></h3>" +
+                    "<h3>인증링크는 5분간 유효합니다.</h3>";
+
+            mimeMessageHelper.setText(sb, true);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendUsernameMail(String email, String title) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(NotFoundMemberException::new);
+
+        String message = "아이디는 " + member.getUsername() + " 입니다.";
+        sendMail(email, title, message);
+    }
+
+
     private Integer generateRandomNumber() {
         return (int) (Math.random() * 999999) + 100000;
     }
@@ -88,7 +135,7 @@ public class MailService {
 
     private String randomUrl() {
         int num = (int) (Math.random() * 3);
-        String sb = "https://d14sxnpwbfro1f.cloudfront.net/email_logo_" +
+        String sb = "https://cdn.artscope.kr/email_logo_" +
             num +
             ".jpeg?w=300&f=webp&q=50";
         return sb;
