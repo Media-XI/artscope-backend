9d7ed609349c500f41da6ae1504987c3755f1ade
gimdonghyeon <haroya01@naver.com>
2024-01-13 14:42:47 +0900

    feat: location 상세조회시 작성자와 생성, 수정 시간 반환 추가

--- src/main/java/com/example/codebase/domain/location/dto/LocationResponseDTO.java
+++ src/main/java/com/example/codebase/domain/location/dto/LocationResponseDTO.java
@@ -1,7 +1,10 @@
 package com.example.codebase.domain.location.dto;
 
 import com.example.codebase.domain.location.entity.Location;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -28,17 +31,55 @@ public class LocationResponseDTO {
 
     private String snsUrl;
 
+    private String authorName;
+
+    private String authorUserName;
+
+    private String authorProfileImage;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime createdTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime updatedTime;
+
     public static LocationResponseDTO from(Location location) {
+        String authorName = null;
+        String authorUserName = null;
+        String authorProfileImage = null;
+        LocalDateTime createdTime = null;
+        LocalDateTime updatedTime = null;
+
+        if (location.getMember() != null) {
+            authorName = location.getMember().getName();
+            authorUserName = location.getMember().getUsername();
+            authorProfileImage = location.getMember().getPicture();
+        }
+
+        if( location.getCreatedTime() != null) {
+            createdTime = location.getCreatedTime();
+        }
+
+        if( location.getUpdatedTime() != null) {
+            updatedTime = location.getUpdatedTime();
+        }
+
+
         return LocationResponseDTO.builder()
-            .id(location.getId())
-            .latitude(location.getLatitude())
-            .longitude(location.getLongitude())
-            .address(location.getAddress())
-            .name(location.getName())
-            .englishName(location.getEnglishName())
-            .phoneNumber(location.getPhoneNumber())
-            .webSiteUrl(location.getWebSiteUrl())
-            .snsUrl(location.getSnsUrl())
-            .build();
+                .id(location.getId())
+                .latitude(location.getLatitude())
+                .longitude(location.getLongitude())
+                .address(location.getAddress())
+                .name(location.getName())
+                .englishName(location.getEnglishName())
+                .phoneNumber(location.getPhoneNumber())
+                .webSiteUrl(location.getWebSiteUrl())
+                .snsUrl(location.getSnsUrl())
+                .authorName(authorName)
+                .authorUserName(authorUserName)
+                .authorProfileImage(authorProfileImage)
+                .createdTime(createdTime)
+                .updatedTime(updatedTime)
+                .build();
     }
 }
