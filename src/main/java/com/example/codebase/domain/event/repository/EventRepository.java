a110833855085f4b6820bd2b26d7e1d6d0150786
gimdonghyeon <haroya01@naver.com>
2024-01-15 17:46:56 +0900

    refactor: 이벤트를 username으로 조회하는 api가 추가됨에 따라 쿼리 변경
    - findEventsByMember()
    - findEventsByEventType()

--- src/main/java/com/example/codebase/domain/event/repository/EventRepository.java
+++ src/main/java/com/example/codebase/domain/event/repository/EventRepository.java
@@ -2,6 +2,7 @@ package com.example.codebase.domain.event.repository;
 
 import com.example.codebase.domain.event.entity.Event;
 import com.example.codebase.domain.event.entity.EventType;
+import com.example.codebase.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,12 +15,14 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT e FROM Event e WHERE e.seq = :seq")
     Optional<Event> findBySeq(Long seq);
 
-    @Query("SELECT e FROM Event e LEFT JOIN e.member m WHERE " +
-            "(:username IS NULL OR m.username LIKE :username%) AND " +
+    @Query("SELECT e FROM Event e WHERE "+
             "e.startDate >= :startDate AND e.endDate <= :endDate AND " +
             "(:eventType IS NULL OR e.type = :eventType) " +
             "ORDER BY e.startDate")
-    Page<Event> findByUserNameAndEventType(String username, LocalDate startDate, LocalDate endDate, EventType eventType, PageRequest pageRequest);
+    Page<Event> findEventsByEventType(LocalDate startDate, LocalDate endDate, EventType eventType, PageRequest pageRequest);
 
 
+    @Query("SELECT e FROM Event e LEFT JOIN e.member m ON e.member = :member " +
+            "ORDER BY e.startDate")
+    Page<Event> findEventsByMember(Member member, PageRequest pageRequest);
 }
