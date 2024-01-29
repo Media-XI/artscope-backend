f0659d08d4d59b7dcb6645f19a658fb892e16b85
gimdonghyeon <haroya01@naver.com>
2024-01-29 12:08:08 +0900

    feat: follow table 생성

---
+++ src/main/resources/db/migration/V202401281030__create_follow_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `follow`
+(
+    follow_id BINARY(16),
+    follower_id BINARY(16),
+    follow_time DATETIME NOT NULL,
+    PRIMARY KEY (follow_id, follower_id),
+    constraint fk_follow_follow_id
+        foreign key (follow_id) references member (member_id),
+    constraint fk_follow_follower_id
+        foreign key (follower_id) references member(member_id)
+) ENGINE =InnoDB
+  DEFAULT CHARSET = utf8mb4;
