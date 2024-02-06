50741d9ad6f6ba2bd6693e411e742afa5625fa43
gimdonghyeon <haroya01@naver.com>
2024-02-06 21:45:36 +0900

    refactor: follow 테이블의 column명 변경 쿼리 통합

--- src/main/resources/db/migration/V202402031753__modifiy_follow_id_name_and_position.sql
+++ src/main/resources/db/migration/V202402031753__modifiy_follow_id_name_and_position.sql
@@ -1,5 +1,3 @@
 ALTER TABLE `follow`
-    CHANGE COLUMN `follower_id` `following_id` BINARY(16) NOT NULL ;
-
-ALTER TABLE `follow`
+    CHANGE COLUMN `follower_id` `following_id` BINARY(16) NOT NULL ,
     CHANGE COLUMN `follow_id` `follower_id` BINARY(16) NOT NULL ;
