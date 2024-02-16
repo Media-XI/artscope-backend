feffe53d4fea13f049244ff7bc4699d41733a3d7
gimdonghyeon <haroya01@naver.com>
2024-02-16 10:38:47 +0900

    feat: RedisUtil에 notificaiton 관련 추가

--- src/main/java/com/example/codebase/util/RedisUtil.java
+++ src/main/java/com/example/codebase/util/RedisUtil.java
@@ -1,10 +1,14 @@
 package com.example.codebase.util;
 
+import com.example.codebase.domain.notification.dto.NotificationResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Optional;
 
@@ -13,9 +17,12 @@ public class RedisUtil {
 
     private final StringRedisTemplate redisTemplate;
 
+    private final ObjectMapper objectMapper;
+
     @Autowired
-    public RedisUtil(StringRedisTemplate redisTemplate) {
+    public RedisUtil(StringRedisTemplate redisTemplate, ObjectMapper objectMapper) {
         this.redisTemplate = redisTemplate;
+        this.objectMapper = objectMapper;
     }
 
     public Optional<String> getData(String key) {
@@ -33,4 +40,27 @@ public class RedisUtil {
     public void deleteData(String key) {
         redisTemplate.delete(key);
     }
+
+    // NotificationMessage
+    public void saveNotificationMessage(String key, NotificationResponse.EventMessage eventMessage, long expireDuration) {
+        try {
+            String json = objectMapper.writeValueAsString(eventMessage);
+            redisTemplate.opsForValue().set(key, json, Duration.ofMillis(expireDuration));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON 변환 중 에러 발생", e);
+        }
+    }
+
+    public NotificationResponse.EventMessage getNotificationMessage(String key) {
+        String json = redisTemplate.opsForValue().get(key);
+        if (json == null) {
+            throw new RuntimeException("저장된 데이터가 없습니다");
+        }
+        try {
+            return objectMapper.readValue(json, NotificationResponse.EventMessage.class);
+        } catch (IOException e) {
+            throw new RuntimeException("JSON 읽는 중 에러 발생", e);
+        }
+    }
+
 }
