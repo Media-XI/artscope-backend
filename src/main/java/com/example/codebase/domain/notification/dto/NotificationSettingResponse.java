05a04f6b1ac16bc4909ab2d184ddfa417ae49670
gimdonghyeon <haroya01@naver.com>
2024-03-01 18:47:09 +0900

    feat: notification_setting 변경 사항에 따른 로직 변경

--- src/main/java/com/example/codebase/domain/notification/dto/NotificationSettingResponse.java
+++ src/main/java/com/example/codebase/domain/notification/dto/NotificationSettingResponse.java
@@ -5,37 +5,27 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 public class NotificationSettingResponse {
 
     @Getter
     @Setter
-    @Schema(name = "NotificationSettingResponse.Get", description = "알림 설정 조회 응답")
-    public static class Get{
-        private String notificationType;
-
-        private Boolean isReceive;
-
-        public static Get from(NotificationSetting notificationSetting){
-            Get response = new Get();
-            response.notificationType = notificationSetting.getNotificationType().name();
-            response.isReceive = notificationSetting.getIsReceive();
-            return response;
-        }
-    }
-
-    @Getter
-    @Setter
+    @Schema(name = "NotificationSettingResponse.GetALL", description = "알림 설정 조회 응답")
     public static class GetAll{
-        private List<Get> notificationSettings;
+        private Boolean receiveMention;
+        private Boolean receiveUpdate;
+        private Boolean receiveAnnouncement;
+        private Boolean receiveNewFollower;
+        private Boolean receivePromotionalNews;
 
-        public static GetAll from(List<NotificationSetting> notificationSettings){
+        public static GetAll from(NotificationSetting notificationSetting){
             GetAll response = new GetAll();
-            response.notificationSettings = notificationSettings.stream()
-                    .map(NotificationSettingResponse.Get::from)
-                    .toList();
+            response.receiveMention = notificationSetting.getReceiveMention();
+            response.receiveUpdate = notificationSetting.getReceiveUpdate();
+            response.receiveAnnouncement = notificationSetting.getReceiveAnnouncement();
+            response.receiveNewFollower = notificationSetting.getReceiveNewFollower();
+            response.receivePromotionalNews = notificationSetting.getReceivePromotionalNews();
             return response;
         }
     }
+
 }
--- src/main/java/com/example/codebase/domain/notification/service/NotificationSettingService.java
+++ src/main/java/com/example/codebase/domain/notification/service/NotificationSettingService.java
@@ -3,14 +3,11 @@ package com.example.codebase.domain.notification.service;
 import com.example.codebase.domain.member.entity.Member;
 import com.example.codebase.domain.notification.dto.NotificationSettingResponse;
 import com.example.codebase.domain.notification.entity.NotificationSetting;
-import com.example.codebase.domain.notification.entity.NotificationSettingIds;
 import com.example.codebase.domain.notification.entity.NotificationType;
 import com.example.codebase.domain.notification.repository.NotificationSettingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class NotificationSettingService {
@@ -24,35 +21,25 @@ public class NotificationSettingService {
 
     @Transactional(readOnly = true)
     public NotificationSettingResponse.GetAll getNotificationSetting(Member member) {
-        List<NotificationSetting> notificationSettings = notificationSettingRepository.findAllByMember(member);
+        NotificationSetting notificationSetting = notificationSettingRepository.findById(member.getId())
+                .orElseThrow(() -> new RuntimeException("알림 설정이 존재하지 않습니다."));
 
-        return NotificationSettingResponse.GetAll.from(notificationSettings);
+        return NotificationSettingResponse.GetAll.from(notificationSetting);
     }
 
     @Transactional
-    public void updateNotificationSetting(String notificationType, Member member) {
-        NotificationType type = NotificationType.valueOf(notificationType);
-        NotificationSettingIds notificationSettingIds = new NotificationSettingIds(member.getId(), type);
-
-        NotificationSetting notificationSetting = notificationSettingRepository.findById(notificationSettingIds)
+    public void updateNotificationSetting(NotificationType type, Member member) {
+        NotificationSetting notificationSetting = notificationSettingRepository.findById(member.getId())
                 .orElseThrow(() -> new RuntimeException("알림 설정이 존재하지 않습니다."));
 
-        notificationSetting.updateReceive();
+        notificationSetting.updateReceive(type);
 
         notificationSettingRepository.save(notificationSetting);
     }
 
+    @Transactional
     public void createNotificationSetting(Member member) {
-        for (NotificationType type : NotificationType.values()) {
-            NotificationSetting notificationSetting = NotificationSetting.builder()
-                    .member(member)
-                    .notificationType(type)
-                    .isReceive(true)
-                    .build();
-
-            notificationSettingRepository.save(notificationSetting);
-        }
+        NotificationSetting notificationSetting = NotificationSetting.from(member);
+        notificationSettingRepository.save(notificationSetting);
     }
-
-
 }
