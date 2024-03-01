241603bf4fb36470fabaaf50291c89740203af5f
Hoon9901 <shonn.dev@gmail.com>
2024-03-01 17:03:15 +0900

    fix: 매거진 미디어 잘못된 Table 지정 수정

--- src/main/java/com/example/codebase/domain/magazine/entity/MagazineMedia.java
+++ src/main/java/com/example/codebase/domain/magazine/entity/MagazineMedia.java
@@ -1,5 +1,6 @@
 package com.example.codebase.domain.magazine.entity;
 
+import com.example.codebase.domain.magazine.repository.MagazineMediaRepository;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,9 +9,11 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
-@Table(name = "magazine")
+@Table(name = "magazine_media")
 @Getter
 @Builder
 @AllArgsConstructor
@@ -22,20 +25,42 @@ public class MagazineMedia {
     @Column(name = "magazine_media_id")
     private Long id;
 
+    enum MagazineMediaType {
+        IMAGE, VIDEO
+    }
+
     @Column(name = "type")
-    private String type;
+    @Enumerated(EnumType.STRING)
+    private MagazineMediaType type;
 
     @Column(name = "url")
     private String url;
 
+    @Builder.Default
     @Column(name = "created_time")
-    private LocalDateTime createdTime;
+    private LocalDateTime createdTime = LocalDateTime.now();
 
+    @Builder.Default
     @Column(name = "updated_time")
-    private LocalDateTime updatedTime;
+    private LocalDateTime updatedTime = LocalDateTime.now();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "magazine_id")
     private Magazine magazine;
 
+    public static MagazineMedia toEntity(String mediaUrl, Magazine newMagazine) {
+        MagazineMedia magazineMedia = MagazineMedia.builder()
+                .type(MagazineMediaType.IMAGE)
+                .url(mediaUrl)
+                .magazine(newMagazine)
+                .build();
+        newMagazine.addMedia(magazineMedia);
+        return magazineMedia;
+    }
+
+    public static List<MagazineMedia> toList(List<String> mediaUrls, Magazine newMagazine) {
+        return mediaUrls.stream()
+                .map(mediaUrl -> MagazineMedia.toEntity(mediaUrl, newMagazine))
+                .toList();
+    }
 }
