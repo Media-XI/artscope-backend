2c5f73dd8d1de2de9651b948a8fe7127ad6ad3a2
gimdonghyeon <haroya01@naver.com>
2024-03-01 20:40:08 +0900

    feat: Notifcation생성시 dto 검증 로직 추가

--- src/main/java/com/example/codebase/domain/notification/dto/NotificationMessageRequest.java
+++ src/main/java/com/example/codebase/domain/notification/dto/NotificationMessageRequest.java
@@ -5,10 +5,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class NotificationMessageRequest {
 
@@ -18,7 +20,7 @@ public class NotificationMessageRequest {
     @NotBlank(message = "내용을 입력해주세요.")
     private String message;
 
-    @NotNull(message = "알림의 타입을 기입해주세요 ")
+    @NotNull(message = "알림 타입을 입력해주세요.")
     private NotificationType notificationType;
 
     public void validAdminNotificationType() {
