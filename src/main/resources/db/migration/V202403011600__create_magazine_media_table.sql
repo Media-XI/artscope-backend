94c340f1c2da99331d541761a251f508fd4f63e4
Hoon9901 <shonn.dev@gmail.com>
2024-03-01 17:17:14 +0900

    fix: magazine_media 테이블의 type 컬럼 varchar 사이즈 변경
    - 255 -> 10 로 변경
    - 가변문자열 타입이지만 길이가 10을 초과할 도메인 값이 아님

--- src/main/resources/db/migration/V202403011600__create_magazine_media_table.sql
+++ src/main/resources/db/migration/V202403011600__create_magazine_media_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `magazine_media`
 (
     `magazine_media_id` bigint PRIMARY KEY AUTO_INCREMENT,
-    `type`              varchar(255) NOT NULL,
+    `type`              varchar(10) NOT NULL,
     `url`               varchar(512) NOT NULL,
     `created_time`      datetime     NOT NULL,
     `updated_time`      datetime     NOT NULL,
