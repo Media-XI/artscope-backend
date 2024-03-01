649ecaecc7551e8ac794d76cd90a42d0cf6af81e
gimdonghyeon <haroya01@naver.com>
2024-03-01 18:17:30 +0900

    refactor: @Controller을 @RestController로 변경

--- src/main/java/com/example/codebase/controller/NotificationSettingController.java
+++ src/main/java/com/example/codebase/controller/NotificationSettingController.java
@@ -4,21 +4,19 @@ import com.example.codebase.annotation.LoginOnly;
 import com.example.codebase.domain.member.entity.Member;
 import com.example.codebase.domain.member.service.MemberService;
 import com.example.codebase.domain.notification.dto.NotificationSettingResponse;
+import com.example.codebase.domain.notification.entity.NotificationType;
 import com.example.codebase.domain.notification.service.NotificationSettingService;
 import com.example.codebase.exception.LoginRequiredException;
 import com.example.codebase.util.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @Tag(name = "NotificationSetting", description = "알림 설정 API")
 @RequestMapping("api/notification-setting")
 public class NotificationSettingController {
@@ -48,7 +46,7 @@ public class NotificationSettingController {
     @LoginOnly
     @Operation(summary = "알림 설정 변경", description = "알림 설정을 변경합니다.")
     @PatchMapping("/{notificationType}")
-    public ResponseEntity updateNotificationSetting(@PathVariable String notificationType) {
+    public ResponseEntity updateNotificationSetting(@PathVariable @Valid NotificationType notificationType) {
         String loginUsername = SecurityUtil.getCurrentUsername().orElseThrow(LoginRequiredException::new);
         Member member = memberService.getEntity(loginUsername);
 
