f2866c050bebf9a06ff02722ac4b105d2a44deb8
Hoon9901 <shonn.dev@gmail.com>
2024-03-04 17:42:06 +0900

    feat: 매거진 생성 및 응답 DTO에 메타데이터 추가

--- src/main/java/com/example/codebase/domain/magazine/dto/MagazineRequest.java
+++ src/main/java/com/example/codebase/domain/magazine/dto/MagazineRequest.java
@@ -10,6 +10,7 @@ import org.hibernate.validator.constraints.URL;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class MagazineRequest {
 
@@ -26,6 +27,9 @@ public class MagazineRequest {
 
         @NotNull
         private Long categoryId;
+
+        // TODO: metadata 에 대해 검증 필요 (보안)
+        private Map<String, String> metadata;
 
         @Size(max = 10, message = "최대 10개 까지 미디어 첨부 가능합니다.")
         private List<@URL(message = "올바른 URL 형식이 아닙니다.") String> mediaUrls = Collections.emptyList();
