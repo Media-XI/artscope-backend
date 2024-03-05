631d23f5dcfec88f588bd67928e5419442efa562
gimdonghyeon <haroya01@naver.com>
2024-03-05 13:28:39 +0900

    refactor: 변경된 curation에 맞춰서 jpa매핑

--- src/main/java/com/example/codebase/domain/curation/entity/Curation.java
+++ src/main/java/com/example/codebase/domain/curation/entity/Curation.java
@@ -1,11 +1,12 @@
 package com.example.codebase.domain.curation.entity;
 
-import com.example.codebase.domain.curation.dto.CurationRequest;
 import com.example.codebase.domain.magazine.entity.Magazine;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
 
@@ -14,30 +15,42 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Where(clause = "is_deleted = false")
 public class Curation {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "magazine_id", unique = true)
+    @JoinColumn(name = "magazine_id")
     private Magazine magazine;
 
+    @Builder.Default
+    @Column(name = "created_time")
+    private LocalDateTime createdTime = LocalDateTime.now();
+
+    @Builder.Default
     @Column(name = "updated_time")
-    private LocalDateTime updatedTime;
+    private LocalDateTime updatedTime = LocalDateTime.now();
 
+    @Builder.Default
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = false;
 
-    public void clearMagazine() {
-        this.magazine = null;
+    public void delete(){
+        this.isDeleted = true;
         this.updatedTime = LocalDateTime.now();
-    }
-
-    public void updateCuration(CurationRequest curationRequest, Curation curation) {
     }
 
     public void setMagazine(Magazine megazine) {
         this.magazine = megazine;
+        this.updatedTime = LocalDateTime.now();
+    }
+
+    public void setUpdatedTime() {
         this.updatedTime = LocalDateTime.now();
     }
 }
