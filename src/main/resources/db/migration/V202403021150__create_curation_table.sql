415dcab73a79e735d097a1becd468b7bf82e2203
gimdonghyeon <haroya01@naver.com>
2024-03-05 19:39:49 +0900

    refactor: is_deleted @Column 제거

--- src/main/resources/db/migration/V202403021150__create_curation_table.sql
+++ src/main/resources/db/migration/V202403021150__create_curation_table.sql
@@ -4,7 +4,6 @@ CREATE TABLE `curation`
     magazine_id  BIGINT   NULL,
     created_time DATETIME NOT NULL,
     updated_time DATETIME NOT NULL,
-    is_deleted   tinyint  NOT NULL DEFAULT 0,
     FOREIGN KEY (`magazine_id`) REFERENCES `magazine` (`magazine_id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
