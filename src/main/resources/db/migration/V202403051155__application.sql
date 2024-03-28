5156004850ab22d2f81bef198247001550d863fd
Hoon9901 <shonn.dev@gmail.com>
2024-03-28 16:00:33 +0900

    feat: Group -> Team 으로 변경됨에 따른 SQL 스크립트 수정

--- src/main/resources/db/migration/V202403051155__application.sql
+++ src/main/resources/db/migration/V202403051155__application.sql
@@ -3,17 +3,17 @@
 # 작성 날짜  : 2024-03-28
 # 현재 버전  : V202403051155 (이전 버전 : V202403051154__application.sql)
 
-CREATE TABLE `group`
+CREATE TABLE `team`
 (
-    group_id               BIGINT PRIMARY KEY AUTO_INCREMENT,
-    description            TEXT,
-    address                varchar(255),
-    profile_image    varchar(512) NOT NULL,
-    background_image varchar(512) NOT NULL,
-    name             varchar(50) NOT NULL,
-    created_time           TIMESTAMP                   NOT NULL,
-    updated_time           TIMESTAMP                   NOT NULL,
-    is_deleted             BOOL DEFAULT FALSE NOT NULL
+    `team_id`          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `description`      TEXT,
+    `address`          varchar(255),
+    `profile_image`    varchar(512)       NOT NULL,
+    `background_image` varchar(512)       NOT NULL,
+    `name`             varchar(50)        NOT NULL,
+    `created_time`     TIMESTAMP          NOT NULL,
+    `updated_time`     TIMESTAMP          NOT NULL,
+    `is_deleted`       BOOL DEFAULT FALSE NOT NULL
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
