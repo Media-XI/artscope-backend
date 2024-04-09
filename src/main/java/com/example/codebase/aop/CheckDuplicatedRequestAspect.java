50177dd49c8e45a37c1bb2f4c5a6295b0cfa10ef
gimdonghyeon <haroya01@naver.com>
2024-04-09 23:58:54 +0900

    feat: CheckDuplicatedRequestAspect 관련 로직 변경 - @CheckDuplicatedRequest 어노테이션 사용시 로그인 관련 어노테이션이 달려있는지 확인 - @CheckDuplicatedRequest target 미 정의시 메서드 명 사용

--- src/main/java/com/example/codebase/aop/CheckDuplicatedRequestAspect.java
+++ src/main/java/com/example/codebase/aop/CheckDuplicatedRequestAspect.java
@@ -1,7 +1,11 @@
 package com.example.codebase.aop;
 
 import com.example.codebase.annotation.CheckDuplicatedRequest;
+import com.example.codebase.annotation.LoginOnly;
+import com.example.codebase.annotation.UserAdminOnly;
+import com.example.codebase.annotation.UserOnly;
 import com.example.codebase.exception.DuplicatedRequestException;
+import com.example.codebase.exception.LoginRequiredException;
 import com.example.codebase.util.RedisUtil;
 import com.example.codebase.util.SecurityUtil;
 import org.aspectj.lang.JoinPoint;
@@ -9,6 +13,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -16,6 +21,7 @@ import java.time.LocalDateTime;
 
 @Aspect
 @Component
+@Profile("!test")
 public class CheckDuplicatedRequestAspect {
 
     private final RedisUtil redisUtil;
@@ -27,25 +33,28 @@ public class CheckDuplicatedRequestAspect {
 
     @Before("@annotation(com.example.codebase.annotation.CheckDuplicatedRequest)")
     public void checkDuplicateRequest(JoinPoint joinPoint) {
-        CheckDuplicatedRequest checkDuplicatedRequest = getAnnotation(joinPoint);
-        String username = String.valueOf(SecurityUtil.getCurrentUsername());
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        CheckDuplicatedRequest annotation = method.getAnnotation(CheckDuplicatedRequest.class);
 
-        if(username.isEmpty()){
-            return;
-        }
+        if (!isValidAnnotation(method)) return;
 
-        String target = checkDuplicatedRequest.target();
+        String username = SecurityUtil.getCurrentUsername().orElseThrow(LoginRequiredException::new);
+
+        if (username.isEmpty()) return;
+
+        String target = annotation.target().isEmpty() ? String.valueOf(method) : annotation.target();
 
         String uniqueKey = username + "->" + target;
         if (redisUtil.getData(uniqueKey).isPresent()) {
             throw new DuplicatedRequestException();
         }
-        redisUtil.setDataAndExpire(uniqueKey, String.valueOf(LocalDateTime.now()), 3000);
+        redisUtil.setDataAndExpire(uniqueKey, String.valueOf(LocalDateTime.now()), annotation.aliveMillisecondTime());
     }
 
-    private CheckDuplicatedRequest getAnnotation(JoinPoint joinPoint) {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
-        return method.getAnnotation(CheckDuplicatedRequest.class);
+    private boolean isValidAnnotation(Method method) {
+        return method.isAnnotationPresent(LoginOnly.class)
+                || method.isAnnotationPresent(UserOnly.class)
+                || method.isAnnotationPresent(UserAdminOnly.class);
     }
 }
