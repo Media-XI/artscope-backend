db4cdd16bf941e6a67dd843e3f50742228b7b388
Hoon9901 <shonn.dev@gmail.com>
2024-04-18 19:36:41 +0900

    chore: 사용하지 않는 API 주석 처리

--- src/main/java/com/example/codebase/controller/MemberController.java
+++ src/main/java/com/example/codebase/controller/MemberController.java
@@ -262,10 +262,11 @@ public class MemberController {
         return new ResponseEntity(message, HttpStatus.OK);
     }
 
-    @Operation(summary = "회원 아이디 전체 조회", description = "검색엔진 노출 용 회원 아이디 리스트를 조회합니다.")
-    @GetMapping("/username")
-    public ResponseEntity getAllUsername() {
-        List<String> usernameList = memberService.getAllUsername();
-        return new ResponseEntity(usernameList, HttpStatus.OK);
-    }
+    // @Operation(summary = "회원 아이디 전체 조회", description = "검색엔진 노출 용 회원 아이디 리스트를
+    // 조회합니다.")
+    // @GetMapping("/username")
+    // public ResponseEntity getAllUsername() {
+    // List<String> usernameList = memberService.getAllUsername();
+    // return new ResponseEntity(usernameList, HttpStatus.OK);
+    // }
 }
